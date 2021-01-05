fc40e979952480a6eafe28921d983a6380df10ff
Marco Friaz <marfriaz@gmail.com>
2021-01-04 23:55:56 -0800

    created migrations

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 module.exports = {
-  migrationsDirectory: "migrations",
+  migrationDirectory: "migrations",
+  driver: "pg",
   connectionString:
     process.env.NODE_ENV === "test"
       ? process.env.TEST_DATABASE_URL
